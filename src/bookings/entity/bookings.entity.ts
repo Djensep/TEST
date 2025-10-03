603e39116256b3ad01deb35c13ffa6e9a21466c7
Lee Djen Sep <leedjensep@gmail.com>
2025-10-03 20:44:48 +0500

    ADD main entities

---
+++ src/bookings/entity/bookings.entity.ts
@@ -0,0 +1,23 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { EventEntity } from './event.entity';
+
+@Entity('bookings')
+export class BookingsEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  user_id: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @ManyToOne(() => EventEntity, (e) => e.id, { onDelete: 'CASCADE' })
+  event: EventEntity;
+}
---
+++ src/bookings/entity/event.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { BookingsEntity } from './bookings.entity';
+
+@Entity('events')
+export class EventEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'int' })
+  total_seats: number;
+
+  @OneToMany(() => BookingsEntity, (b) => b.event)
+  userId: BookingsEntity;
+}
