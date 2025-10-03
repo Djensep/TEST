c12ce6383a6b387ca190a1324ba4a601c9a92c5a
Lee Djen Sep <leedjensep@gmail.com>
2025-10-03 20:29:52 +0500

    ADD new bookings module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [],
+  imports: [BookingsModule],
   controllers: [],
   providers: [],
 })
